e40d3feefc67eecce48208d4eb749e840f44c143
poiuynumi <jeon1273@naver.com>
2018-12-07 09:02:50 +0900

    .

--- snewdeal-project-48/src/main/java/com/eomcs/lms/handler/Command.java
+++
@@ -1,7 +0,0 @@
-package com.eomcs.lms.handler;
-
-// Caller: App 클래스
-// Callee: 명령 처리기 
-public interface Command {
-  void execute();
-}
--- snewdeal-project-54/src/main/java/com/eomcs/lms/dao/MemberDao.java
+++
@@ -1,8 +0,0 @@
-package com.eomcs.lms.dao;
-
-import com.eomcs.lms.domain.Member;
-
-public interface MemberDao {
-  Member findByEmailPassword(String email, String password) 
-      throws Exception;
-}
