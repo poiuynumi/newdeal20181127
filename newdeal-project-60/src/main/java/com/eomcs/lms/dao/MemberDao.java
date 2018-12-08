c8aa1af2d504264fde6eb2eac4f21f3418ce9edd
poiuynumi <jeon1273@naver.com>
2018-12-08 18:32:46 +0900

    '.'

--- newdeal-project-60/src/main/java/com/eomcs/lms/dao/MemberDao.java
+++
@@ -1,9 +0,0 @@
-package com.eomcs.lms.dao;
-
-import java.util.Map;
-import com.eomcs.lms.domain.Member;
-
-public interface MemberDao {
-  Member findByEmailPassword(Map<String, Object> params) 
-      throws Exception;
-}
